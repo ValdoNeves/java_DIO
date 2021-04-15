cd4d33417a1c35dfa10d4be420a76d0a2d93c9e5
Valdo Neves <valdogt1@gmail.com>
2021-04-15 18:10:27 -0300

    iniciando testes com maven e gradle

---
+++ exemplo-gradle/src/test/java/com/orgfree/valdoneves/exemplogradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orgfree.valdoneves.exemplogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ exemplo-maven/src/test/java/com/orgfree/valdoneves/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orgfree.valdoneves.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
