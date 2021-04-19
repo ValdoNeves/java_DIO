7ac2184d37fc07188df2f2d197caf031a0942098
Valdo Neves <valdogt1@gmail.com>
2021-04-19 00:02:59 -0300

    configurando pmd

---
+++ basicoJava/src/main/java/com/orgfree/valdoneves/strings/Strings.java
@@ -0,0 +1,14 @@
+package com.orgfree.valdoneves.strings;
+
+public class Strings {
+
+    public static void main(String[] args){
+
+        String nome = "V a l d o";
+        var sobreNome = "Neves";
+
+//        System.out.println(String.format("Meu nome é %s %s", nome , sobreNome));
+
+        System.out.println("1234 ASDA QW".replaceAll("[0-9]","#"));
+    }
+}
