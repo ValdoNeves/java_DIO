51a584f68d470e76cec0156c110042a25f0c221f
Valdo Neves <valdogt1@gmail.com>
2021-04-24 01:20:26 -0300

    fatorial recursivo e tail call

---
+++ basicoJava/src/main/java/com/orgfree/valdoneves/fatorialRecursivo/FatorialTailCall.java
@@ -0,0 +1,16 @@
+package com.orgfree.valdoneves.fatorialRecursivo;
+
+public class FatorialTailCall {
+    public static void main(String[] args) {
+        System.out.println(fatorialA(5));
+    }
+    public static double fatorialA(double valor){
+        return fatorialComTailCall(valor,1);
+    }
+    public static double fatorialComTailCall(double valor, double numero){
+        if(valor == 0){
+            return numero;
+        }
+        return fatorialComTailCall(valor-1,numero*valor);
+    }
+}
