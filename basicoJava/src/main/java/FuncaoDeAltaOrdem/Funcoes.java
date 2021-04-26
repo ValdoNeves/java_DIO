8c820de8462a613e8be98ef5e1234c4b26c2f79e
Valdo Neves <valdogt1@gmail.com>
2021-04-25 23:56:53 -0300

    entendendo a API Function<Object, Object>

--- basicoJava/src/main/java/FuncaoDeAltaOrdem/Funcoes.java
+++ basicoJava/src/main/java/FuncaoDeAltaOrdem/Funcoes.java
@@ -6,5 +6,13 @@ public class Funcoes {
     public static void main(String[] args) {
         Function<String, String> retornaNomeAoContrario = texto -> new StringBuilder(texto).reverse().toString();
         System.out.println(retornaNomeAoContrario.apply("Teste"));
+
+        //as duas formas fazem exatamente a mesma coisa
+        //recebe um valor String e o converte em Inteiro
+        Function<String, Integer> converteStringEmInteger = Integer::valueOf;
+        Function<String, Integer> converteStringEmInteger2 = string -> Integer.valueOf(string);
+
+//        Converte a String em Inteiro e multiplica por 2
+        System.out.println(converteStringEmInteger.apply("10") * 2);
     }
 }
